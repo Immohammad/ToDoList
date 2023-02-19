001f3f4a00031c90e06c248ba41c35efd3303e91
Mohammad <mo.movahedinia@gmail.com>
2023-02-19 07:51:01 +0330

    before local storage

---
+++ src/useLocalStorage.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const useLocalStorage = (storageKey, fallbackState) => {
+  const [value, setValue] = useState(
+    JSON.parse(localStorage.getItem(storageKey)) ?? fallbackState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(value));
+  }, [value, storageKey]);
+
+  return [value, setValue];
+};
+export default useLocalStorage;
